408e77d435d07c3cfc11e4792b91fab710c14999
bar9 <rolandbrand11@gmail.com>
2025-03-22 15:04:31 +0100

    updated readme & committed small resources

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Bäumige Beiträge
 
 Im Rahmen der Challenge [Bäumige Direktzahlungen](https://hack.farming.opendata.ch/project/151) an den Open Farming Hackdays 2025 entwickelt.
+
+# Voraussetzungen
+* Rust installieren mit Rustup
+
+# Getting Started
+* Ordner `data` anlegen im Projektverzeichnis
+* Download SWISSTLM3D_2025.gpkg
+* (optional überschreiben) Erstellen eines Shapefiles `outline_liebegg.shp` mit zugehörigen files im Ordner `data` (ohne Inhalt, wird verwendet für Dimensionen des Ausschnitts)
+* (optional überschreiben) export eines Bildes `background_liebegg.png` im `data/`-Verzeichnis von `https://wms.geo.admin.ch/?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&BBOX=2651438.9502558867,1243180.509409728,2651848.0412982227,1243333.9185506043&CRS=EPSG:2056&WIDTH=1024&HEIGHT=384&LAYERS=ch.swisstopo.pixelkarte-farbe&FORMAT=image/png`
+* `cargo run`
+
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use gdal::vector::{Geometry, LayerAccess};
 use geo::{EuclideanDistance, Point};
 use image::{DynamicImage, ImageReader};
 use minifb::{Window, WindowOptions, Key, MouseButton, MouseMode};
+use minifb::Key::P;
 
 fn main() {
     let gpkg_path = "data/SWISSTLM3D_2025.gpkg";
@@ -36,7 +37,7 @@ fn main() {
                     layer.set_spatial_filter(&bounding_box_geometry);
 
                     // Extract tree geometries (simplified for example)
-                    let trees: Vec<Tree> = layer.features().filter_map(|feature| {
+                    let mut trees: Vec<Tree> = layer.features().filter_map(|feature| {
                         if let Some(geometry) = feature.geometry() {
                             Some(Tree { geom: geometry.clone() })
                         } else {
@@ -70,6 +71,8 @@ fn main() {
 
 
                         if let Some((x, mut y)) = window.get_mouse_pos(MouseMode::Clamp) {
+
+
                             // y = 300. - y;
 
                             let img_width = img.width() as usize;
@@ -77,6 +80,7 @@ fn main() {
                             let mut buffer: Vec<u32> = vec![0xFFFFFF; 800 * 300];
                             let mut mouse_down_last_frame = false;
                             let mouse_down = window.get_mouse_down(MouseButton::Left);
+
 
                             // Copy image pixels to buffer, resizing if necessary
                             for y in 0..300 {
@@ -97,11 +101,15 @@ fn main() {
                                 let tx = ((map_x - minx) / (maxx - minx) * 800.0) as i32;
                                 let ty = ((maxy - map_y) / (maxy - miny) * 300.0) as i32;
                                 if tx >= 0 && tx < 800 && ty >= 0 && ty < 300 {
-                                    buffer[(ty as usize * 800 + tx as usize)] = 0xFF0000; // Red color
-                                    buffer[((ty+1) as usize * 800 + tx as usize)] = 0xFF0000; // Red color
-                                    buffer[(ty as usize * 800 + (tx-1) as usize)] = 0xFF0000; // Red color
-                                    buffer[((ty-1) as usize * 800 + tx as usize)] = 0xFF0000; // Red color
-                                    buffer[(ty as usize * 800 + (tx+1) as usize)] = 0xFF0000; // Red color
+                                    buffer[(ty as usize * 800 + tx as usize)] = 0x000000;
+                                    buffer[((ty+1) as usize * 800 + tx as usize)] = 0x000000;
+                                    buffer[((ty+2) as usize * 800 + tx as usize)] = 0x000000;
+                                    buffer[(ty as usize * 800 + (tx-1) as usize)] = 0x000000;
+                                    buffer[(ty as usize * 800 + (tx-2) as usize)] = 0x000000;
+                                    buffer[((ty-1) as usize * 800 + tx as usize)] = 0x000000;
+                                    buffer[((ty-2) as usize * 800 + tx as usize)] = 0x000000;
+                                    buffer[(ty as usize * 800 + (tx+1) as usize)] = 0x000000;
+                                    buffer[(ty as usize * 800 + (tx+2) as usize)] = 0x000000;
                                 }
                             }
 
@@ -115,6 +123,11 @@ fn main() {
                             let new_tree = Tree {
                                 geom: Geometry::from_wkt(&format!("POINT({} {})", spatial_x, spatial_y)).unwrap(),
                             };
+
+                            if (mouse_down && !mouse_down_last_frame) {
+                                trees.push(new_tree.clone());
+                                mouse_down_last_frame = true;
+                            }
 
                             // if mouse_down && !mouse_down_last_frame {
                             // if mouse_down {
@@ -154,67 +167,96 @@ fn heatmap_step_30m(new_tree: Tree, trees: &[Tree], mut buffer: Vec<u32>,
             let xxx = (xx as f64 / 800.) * (maxx - minx) + minx;
             let yyy = ((299 - yy) as f64 / 300.) * (maxy - miny) + miny;
 
-            for y in changed_trees.iter() {
-                if Point::new(xxx, yyy).euclidean_distance(
-                    &Point::new(y.geom.get_point(0).0, y.geom.get_point(0).1)
-                ) < 30. {
-                    for dy in 0..cluster_size {
-                        for dx in 0..cluster_size {
-                            let x_idx = xx + dx;
-                            let y_idx = yy + dy;
-                            if x_idx < 800 && y_idx < 300 {
-                                let i = (x_idx + y_idx * 800) as usize;
-                                let mut color = buffer[i];
-                                let alpha = (color >> 24) & 0xFF;
-                                let mut red = (color >> 16) & 0xFF;
-                                let mut green = (color >> 8) & 0xFF;
-                                let mut blue = color & 0xFF;
+            let mut value = 0;
+            let dists_inv = changed_trees.iter().map(|tree| Point::new(
+                tree.geom.get_point(0).0,
+                tree.geom.get_point(0).1
+            ).euclidean_distance(&Point::new(xxx, yyy)))
+                .map(|x| 1./x as f32)
+                .collect::<Vec<f32>>();
 
-                                red = (red as f32 * 2.).min(255.0) as u32;
-                                green = (green as f32 * 0.5).min(255.0) as u32;
-                                blue = (blue as f32 * 0.5).min(255.0) as u32;
+            let dists_total_inv: f32 = dists_inv.iter().sum::<f32>();
+            // println!("{:?}", dists_total_inv);
 
-                                ret_buffer[i] = (alpha << 24) | (red << 16) | (green << 8) | blue;
-                            }
+            let mut value: f32 = 1. - dists_total_inv;
+            for dy in 0..cluster_size {
+                for dx in 0..cluster_size {
+                    let x_idx = xx + dx;
+                    let y_idx = yy + dy;
+                    if x_idx < 800 && y_idx < 300 {
+                        let i = (x_idx + y_idx * 800) as usize;
+                        let mut color = buffer[i];
+                        // let alpha = (color >> 24) & 0xFF;
+                        let mut red = (color >> 16) & 0xFF;
+                        let mut green = (color >> 8) & 0xFF;
+                        let mut blue = color & 0xFF;
+
+                        // println!("value: {}", value);
+
+
+                        if value >= 0. && value <= 1. {
+                            // Recolor increasingly red if the value is close to 1, or green if close to 0
+                            // Scale red: as value increases, it becomes more red
+                            // red = (red as f32 * (1. + (value * 255.))).min(255.0) as u32;
+                            // Scale green: as value decreases, it becomes more green
+                            // green = (green as f32 * (1. - (value * 255.))).min(255.0) as u32;
+                            // Optionally adjust blue here if needed
+                            // blue = (blue as f32 * (1. - (value * 255.))).min(255.0) as u32;
+                        } else {
+                            // If the value is out of range (less than 0 or greater than 1), max out green
+                            // red = 255; // Maximize green
+                            // println!("value under 0 or over 1");
+                            // Optionally modify red or blue here depending on your requirements
+                            // red = some_value;
+                            // blue = some_value;
                         }
+
+                        let diff_to_full_green= 255 - green;
+                        let plus_green = (diff_to_full_green as f32 * value) as u32;
+                        green = green + plus_green;
+
+                        let diff_to_zero_red = red;
+                        let minus_red = (diff_to_zero_red as f32 * value) as u32;
+                        red = red - minus_red;
+
+                        let diff_to_zero_blue = blue;
+                        let minus_blue = (diff_to_zero_blue as f32 * value) as u32;
+                        blue = blue - minus_blue;
+
+                        // ret_buffer[i] = (red << 16) | (green << 8) | blue;
+                        ret_buffer[i] = (red << 16) | (green << 8) | blue;
                     }
                 }
             }
+
+            // for y in changed_trees.iter() {
+            //     if Point::new(xxx, yyy).euclidean_distance(
+            //         &Point::new(y.geom.get_point(0).0, y.geom.get_point(0).1)
+            //     ) < 30. {
+            //         for dy in 0..cluster_size {
+            //             for dx in 0..cluster_size {
+            //                 let x_idx = xx + dx;
+            //                 let y_idx = yy + dy;
+            //                 if x_idx < 800 && y_idx < 300 {
+            //                     let i = (x_idx + y_idx * 800) as usize;
+            //                     let mut color = buffer[i];
+            //                     let alpha = (color >> 24) & 0xFF;
+            //                     let mut red = (color >> 16) & 0xFF;
+            //                     let mut green = (color >> 8) & 0xFF;
+            //                     let mut blue = color & 0xFF;
+            //
+            //                     red = (red as f32 * 2.).min(255.0) as u32;
+            //                     green = (green as f32 * 0.5).min(255.0) as u32;
+            //                     blue = (blue as f32 * 0.5).min(255.0) as u32;
+            //
+            //                     ret_buffer[i] = (alpha << 24) | (red << 16) | (green << 8) | blue;
+            //                 }
+            //             }
+            //         }
+            //     }
+            // }
         }
     }
-
-    // for (i, mut pixel) in buffer.into_iter().enumerate() {
-    //     let xx = (i as f64 % 800.) as u32;
-    //     let mut yy = (i as f64 / 800.) as u32;
-    //     let xxx = (xx as f64 / 800.) * (maxx - minx) + minx;
-    //     let yyy = ((299-yy) as f64 / 300.) * (maxy - miny) + miny;
-    //     for (j, &y) in changed_trees.iter().enumerate() {
-    //         // if i != j {
-    //         //     println!("{}, {} | {}, {}", xx, yy, xxx, yyy);
-    //         // yy = 299 - yy;
-    //             if Point::new(xxx, yyy).euclidean_distance(
-    //                 &Point::new(y.geom.get_point(0).0, y.geom.get_point(0).1)
-    //             ) < 30. {
-    //                 let mut color = pixel;
-    //                 let alpha = (color >> 24) & 0xFF;
-    //                 let mut red = (color >> 16) & 0xFF;
-    //                 let green = (color >> 8) & 0xFF;
-    //                 let blue = color & 0xFF;
-    //
-    //                 red = (red as f32 * 5.).min(255.0) as u32;
-    //
-    //                 ret_buffer[(xx + yy * 800) as usize] = (alpha << 24) | (red << 16) | (green << 8) | blue;
-    //                 // println!("painting {}/{}", xx, yy);
-    //                 // ret_buffer[(xx*800 + yy) as usize] = 0x000000;
-    //
-    //                 // println!("should paint red at {}, {}", xx, yy);
-    //                 // } else {
-    //                 //     ret_buffer[(xx + yy*800) as usize] = 0;
-    //                 //println!("should not paint red");
-    //             }
-    //         // }
-    //     }
-    // }
 
     ret_buffer
 }
